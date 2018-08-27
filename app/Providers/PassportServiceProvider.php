b7d28ed080eb15226cd4eae5bb2f2d5e3123378e
liuzq <liuzq@novastar.tech>
2018-08-27 10:05:40 +0800

    覆写passport服务提供者

--- app/Providers/PassportServiceProvider.php
+++ app/Providers/PassportServiceProvider.php
@@ -4,10 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\PassportServiceProvider as Passport;
-use App\Passport\Grant\AuthCodeGrant;
-use Laravel\Passport\Bridge\AuthCodeRepository;
-use Laravel\Passport\Bridge\RefreshTokenRepository;
-use DateInterval;
 
 class PassportServiceProvider extends Passport{
 
