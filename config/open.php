9ce9db92cf6a42adecb0a9404789ff98524505b0
liuzq <liuzq@novastar.tech>
2018-08-27 14:10:16 +0800

    覆写认证校验

--- config/open.php
+++ config/open.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'token_expire_in' => 2,//分钟
+    'token_expire_in' => 120,//分钟
     'refresh_token_expire_in' => 30,//天
     'scopes' => [
         'user-userInfo' => 'get user info',
