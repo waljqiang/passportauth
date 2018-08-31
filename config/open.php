92d9f0fbfc76ac28cbfd86974dfdcad04491e7d8
liuzq <liuzq@novastar.tech>
2018-08-31 15:40:51 +0800

    添加获取单个屏体监控数据权限

--- config/open.php
+++ config/open.php
@@ -7,8 +7,8 @@ return [
     'token_expire_in' => 120,//分钟
     'refresh_token_expire_in' => 30,//天
     'scopes' => [
-        'user-userInfo' => 'get user info',
-        'screen-realTime' => 'get realtime of the screen',
-        'screen-getScreenList' => 'get screen list'
+        'user-tags' => 'Get the tags of the user',//获取用户标签列表
+        'screen-spotcheck' => 'Get the spotcheck data of the screen for user',//获取单个屏体点检数据
+        'screen-monitor' => 'Get the monitor data of the screen for user',//获取单个屏体监控数据
     ]
 ];
