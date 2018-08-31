631fbb327553cfd7a57c6ce3be3ec607d1f95e7f
liuzq <liuzq@novastar.tech>
2018-08-31 17:34:38 +0800

    添加获取显示屏列表权限

--- config/open.php
+++ config/open.php
@@ -8,7 +8,7 @@ return [
     'refresh_token_expire_in' => 30,//天
     'scopes' => [
         'user-tags' => 'Get the tags of the user',//获取用户标签列表
-        'screen-list' => 'Get the screens of the user',//获取用户显示屏列表
+        'screen-lists' => 'Get the screens of the user',//获取用户显示屏列表
         'screen-spotcheck' => 'Get the spotcheck data of the screen for user',//获取单个屏体点检数据
         'screen-monitor' => 'Get the monitor data of the screen for user',//获取单个屏体监控数据
     ]
